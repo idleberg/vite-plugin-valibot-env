42acf4bec63beb8f2bca827ad090edf2f4a74dfe
Jan T. Sott <git@idleberg.com>
2024-04-28 09:13:28 +0200

    update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-	target: 'esnext',
-  clean: true,
-  dts: true,
-  entry: ['index.ts'],
-  format: 'esm',
-  minify: true,
-  treeshake: 'recommended'
+export default defineConfig((options) => {
+	return {
+		target: 'esnext',
+		clean: true,
+		dts: !options.watch,
+		entry: ['index.ts'],
+		format: 'esm',
+		minify: !options.watch,
+		treeshake: 'recommended'
+	}
 });
