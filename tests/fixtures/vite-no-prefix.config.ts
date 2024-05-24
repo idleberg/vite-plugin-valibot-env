4beb89f2ee43bea9dbabf1a2773706726ac8d8f3
Jan T. Sott <git@idleberg.com>
2024-05-24 23:57:57 +0200

    add test for ignoreEnvPrefix option

---
+++ tests/fixtures/vite-no-prefix.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import * as v from 'valibot';
+import valibotPlugin from '../../index';
+
+const envSchema = v.object({
+	NODE_ENV: v.literal('development'),
+	VITE_STRING: v.string(),
+});
+
+export default defineConfig({
+	plugins: [
+		valibotPlugin(envSchema, {
+			ignoreEnvPrefix: true
+		})
+	]
+});
---
+++ tests/no-prefix.spec.ts
@@ -0,0 +1,31 @@
+import { execa, type ExecaError } from 'execa';
+import { resolve } from 'path';
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
+
+const viteArgs = [
+	'vite',
+	'build',
+	'--config',
+	'vite-no-prefix.config.ts'
+];
+
+const validEnvironmentVariables = {
+	NODE_ENV: 'development',
+	VITE_STRING: 'DEADBEEF',
+};
+
+test(`Testing valid environment variables`, async () => {
+	try {
+		await execa('npx', viteArgs, {
+			cwd: resolve(process.cwd(), 'tests/fixtures'),
+			env: validEnvironmentVariables
+		});
+	} catch (error) {
+		throw new Error((error as ExecaError).message);
+	}
+
+	assert.ok(true);
+});
+
+test.run();
