3fefdbed8197751b3badce63c4dc0292aae82ab8
Jan T. Sott <git@idleberg.com>
2024-05-28 23:16:31 +0200

    add test for transform option

---
+++ tests/fixtures/vite-transform.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import * as v from 'valibot';
+import valibotPlugin from '../../index';
+
+const envSchema = v.object({
+	VITE_TRUE: v.boolean(),
+	VITE_FALSE: v.boolean(),
+	VITE_NULL: v.null_(),
+	VITE_INT: v.number([v.integer()]),
+	VITE_FLOAT: v.number(),
+});
+
+export default defineConfig({
+	plugins: [
+		valibotPlugin(envSchema, {
+			transform: true,
+		}),
+	],
+});
---
+++ tests/transform.spec.ts
@@ -0,0 +1,31 @@
+import { cwd } from 'node:process';
+import { execa, type ExecaError } from 'execa';
+import { resolve } from 'node:path';
+import { test } from 'uvu';
+import { viteBuild } from './helper';
+import * as assert from 'uvu/assert';
+
+const { command, args } = viteBuild('vite-transform.config.ts');
+
+const validEnvironmentVariables = {
+	VITE_TRUE: 'true',
+	VITE_FALSE: 'false',
+	VITE_NULL: 'null',
+	VITE_INT: '1',
+	VITE_FLOAT: '1.1',
+};
+
+test(`Testing valid environment variables`, async () => {
+	try {
+		await execa(command, args, {
+			cwd: resolve(cwd(), 'tests/fixtures'),
+			env: validEnvironmentVariables,
+		});
+	} catch (error) {
+		throw new Error((error as ExecaError).message);
+	}
+
+	assert.ok(true);
+});
+
+test.run();
